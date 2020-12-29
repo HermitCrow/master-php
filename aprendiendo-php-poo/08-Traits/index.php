8a772116693987f0b6251970bf33c09e8662bf3f
emmanuel.ulloa <emmanuel.ulloa@PGRREDNA150249.mp.dc.pgr.gob.do>
2020-12-29 12:09:30 -0400

    Create Abstract class and create interface and create traits

---
+++ aprendiendo-php-poo/08-Traits/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Coche{
+    public $nombre;
+    public $marca;
+
+}
+
+class Persona{
+    public $nombre;
+    public $apellido;
+}
+
+class VideoJuego{
+    public $nombre;
+    public $anio;
+}
