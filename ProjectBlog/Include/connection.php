d8d2fce2ef45a0475c432e624d8633896512d6cc
emmanuel.ulloa <emmanuel.ulloa@PGRREDNA150249.mp.dc.pgr.gob.do>
2021-03-23 14:39:49 -0400

    Reparando el proyecto

--- ProjectBlog/Include/connection.php
+++ ProjectBlog/Include/connection.php
@@ -1,14 +1,19 @@
 <?php
 
 //Connection Database
-$ServerName = "LAPTOP-J533FSR0";
+/*
+ * $ServerName = "PGRREDNA150249";
+ * $ServerName = "LAPTOP-J533FSR0";
+  */
+$ServerName = "PGRREDNA150249";
 /*$DataBase = "Blog";
 $User = "sa";
 $PassWord = "123456";*/
 $ConnectionString = array("Database" => "Blog", "UID" => "sa", "PWD" => "123456");
 
-$DataContext = sqlsrv_connect($ServerName,$ConnectionString);
-sqlsrv_query($DataContext,"SET NAME 'utf-8'");
+    $DataContext = sqlsrv_connect($ServerName,$ConnectionString);
+    sqlsrv_query($DataContext,"SET NAME 'utf-8'");
+
 
 //Seccion start.
 
