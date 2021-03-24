65865865390250184d6074be43b85c775d0e2c7f
emmanuel.ulloa <emmanuel.ulloa@PGRREDNA150249.mp.dc.pgr.gob.do>
2021-03-24 18:22:59 -0400

    Se inclulleron archivos para la funcionavilidad de la web

--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -1,26 +1,43 @@
 <?php
-
+session_start();
 require_once 'autoload.php';
+require_once 'config/db.php';
+require_once 'config/parametros.php';
+require_once 'helper/utils.php';
+require_once 'Views/layout/header.php';
+require_once 'Views/layout/sidebar.php';
 
 
+function Show_errore(){
+    $error = new errorController();
+    $error->index();
+}
 if (isset($_GET['controller'])) {
     $ControllerName = $_GET['controller'] . 'Controller';
+    
+
+}elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+    $ControllerName = controller_default;
 } else {
-    echo "La pagina que buscas no existe.";
+    
+    Show_errore();
     exit();
 }//Fin If else    
 
 if (class_exists($ControllerName)) {
     $controller = new $ControllerName();
-    
+
     if (isset($_GET['action']) && method_exists($controller, $_GET['action'])) {
         $action = $_GET['action'];
         $controller->$action();
+    }elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+        $action = action_default;
+        $controller->$action();
     } else {
-        echo "La pagina que buscas no existe.";
+        Show_errore();
     }//Fin If else
 } else {
-    echo "La pagina que buscas no existe.";
+    Show_errore();
 }//Fin If else
 
-
+require_once 'Views/layout/footer.php';
