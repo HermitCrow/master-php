7ca2e47092631b8d8fe0a6054da47dabda3be4e2
emmanuel.ulloa <emmanuel.ulloa@PGRREDNA150249.mp.dc.pgr.gob.do>
2021-03-24 18:11:54 -0400

    Se creo la vista destacados del productoController

---
+++ proyecto-php-poo/Views/producto/destacados.php
@@ -0,0 +1,34 @@
+<h1>Productos Destacados</h1>
+
+<div class="product">
+    <img src="assets/img/camiseta.png"/>
+    <h2>Camiseta Azul</h2>
+    <p>250 pesos</p>
+    <a href="#" class="button">Comprar</a>
+</div>
+<div class="product">
+    <img src="assets/img/camiseta.png"/>
+    <h2>Camiseta Azul</h2>
+    <p>250 pesos</p>
+    <a href="#" class="button">Comprar</a>
+</div>
+<div class="product">
+    <img src="assets/img/camiseta.png"/>
+    <h2>Camiseta Azul</h2>
+    <p>250 pesos</p>
+    <a href="#" class="button">Comprar</a>
+</div>
+<div class="product">
+    <img src="assets/img/camiseta.png"/>
+    <h2>Camiseta Azul</h2>
+    <p>250 pesos</p>
+    <a href="#" class="button">Comprar</a>
+</div>
+<div class="product">
+    <img src="assets/img/camiseta.png"/>
+    <h2>Camiseta Azul</h2>
+    <p>250 pesos</p>
+    <a href="#" class="button">Comprar</a>
+</div>
+</div>
+
